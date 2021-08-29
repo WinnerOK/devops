2cb757bb322cf5fd5c1eee3d7ab3814c74260765
WinnerOK <danman339@gmail.com>
2021-08-29 14:19:00 +0300

    start terraform

---
+++ terraform/vbox.tf
@@ -0,0 +1,12 @@
+resource "virtualbox_vm" "node" {
+  count     = 2
+  name      = format("node-%02d", count.index + 1)
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus      = 2
+  memory    = "512 mib"
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "vboxnet1"
+  }
+}
