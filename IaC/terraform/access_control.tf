d2dcbd732eaf68ea923efcc4cbcee28bc57220c7
WinnerOK <danman339@gmail.com>
2021-09-03 20:55:31 +0300

    adapt tf for outer port

--- IaC/terraform/access_control.tf
+++ IaC/terraform/access_control.tf
@@ -30,6 +30,15 @@ resource "aws_security_group" "jenkins" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "python_app"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   egress {
     from_port        = 0
