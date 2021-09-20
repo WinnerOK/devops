3ea1f337a9a0acc49d804495d2f0e3eaf9dd4d17
WinnerOK <danman339@gmail.com>
2021-09-20 14:09:42 +0300

    trigger new cache

--- app_python/main.py
+++ app_python/main.py
@@ -11,5 +11,8 @@ app = FastAPI(
 app.include_router(time_router, prefix="")
 
 Instrumentator().instrument(app).expose(
-    app, endpoint="/metrics", include_in_schema=True, should_gzip=False
+    app,
+    endpoint="/metrics",
+    include_in_schema=True,
+    should_gzip=False,
 )
